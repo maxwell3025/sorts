9637d46394e90ce961d88c6e50f0e23cf6d68116
maxwell3025 <max@itarchi.com>
2019-09-24 19:02:38 -0400

    initial draft

---
+++ src/sorts/selectionwindow.java
@@ -0,0 +1,24 @@
+package sorts;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class selectionwindow extends JFrame implements ActionListener{
+
+	public selectionwindow() {
+		setSize(1920,1080);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		selectionwindow w = new selectionwindow();
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		e.g
+		
+	}
+
+}
